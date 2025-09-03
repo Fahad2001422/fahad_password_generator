d50fce96176d1c9db5962978101c43883d9fdb69
Fahad2001422 <127978441+Fahad2001422@users.noreply.github.com>
2025-09-03 19:08:14 +0300

    For every character of the password, the function "password_generation::generate_random_character" is called, and does the following: 	1. Choose a random Unicode group before generating the character, as implemented in the private "password_generation::choose_unicode_group" function.
    	2. It generates a random number from the chosen Unicode group using another private function, which is not implemented yet.
    
    	3. Finally, It converts the random number into a UTF-8 character and, then, returns that character. This is also not implemented yet.

--- src/password_generation/mod.rs
+++ src/password_generation/mod.rs
@@ -1,14 +1,41 @@
+use rand::{Rng, RngCore};
+use rand::rngs::ThreadRng;
+
 /// The enum `Unicode Group` represents which characters of an alphabet
 /// (e.g. the Unicode groups `Basic_latin`, `Latin_1_Supplement` and `Latin_Extended`)
 /// can be used while generating the random password.
 pub enum UnicodeGroup {
-    Basic_Latin,
-    Latin_1_Supplement,
-    Latin_Extended,
+    BasicLatin,
+    Latin1Supplement,
+    LatinExtended,
     Greek,
     Cyrillic,
     Arabic,
     Hiragana,
     Katakana,
     Hangul
+}
+
+/// This function borrows a ThreadRng both to choose a random Unicode group
+/// and to generate a random character from that group.
+pub fn generate_random_character(rng: &mut ThreadRng) -> char {
+    let unicode_grp_chosen = choose_unicode_group(rng
+        .next_u32());
+}
+
+/// This function returns a Unicode group to generate a character from
+/// based on the `number` passed.
+fn choose_unicode_group(number: u32) -> Option<UnicodeGroup> {
+    match number % 9 {
+        0 => Some(UnicodeGroup::BasicLatin),
+        1 => Some(UnicodeGroup::Latin1Supplement),
+        2 => Some(UnicodeGroup::LatinExtended),
+        3 => Some(UnicodeGroup::Greek),
+        4 => Some(UnicodeGroup::Cyrillic),
+        5 => Some(UnicodeGroup::Arabic),
+        6 => Some(UnicodeGroup::Hiragana),
+        7 => Some(UnicodeGroup::Katakana),
+        8 => Some(UnicodeGroup::Hangul),
+        _ => None
+    }
 }
